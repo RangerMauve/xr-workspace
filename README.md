85160cbdbb3a8f27bf37770ce86b92a66f26d0cf
mauve <ranger@mauve.moe>
2020-05-29 02:45:52 -0400

    Testing xrpackage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # xr-workspace
 My webxr workspace using XRPackage
+
+## Setup
+
+Need to copy over sw.js from the xrpackage repo and add it manually.
---
+++ script.js
@@ -0,0 +1,13 @@
+import {XRPackageEngine, XRPackage} from 'https://xrpackage.org/xrpackage.js';
+
+run()
+
+async function run() {
+	const pe = new XRPackageEngine();
+	document.body.appendChild(pe.domElement);
+
+	const res = await fetch('./packages/xr-terminal.wbn'); // built package stored somewhere
+	const arrayBuffer = await res.arrayBuffer();
+	const p = new XRPackage(new Uint8Array(arrayBuffer));
+	pe.add(p);
+}
