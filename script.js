bc55a7c5c002b10b3588231bc4900a261ab79a19
mauve <ranger@mauve.moe>
2020-06-06 00:01:59 -0400

    Add proxying of keyboard events

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ async function run() {
 	const res = await fetch('./packages/xr-terminal.wbn'); // built package stored somewhere
 	const arrayBuffer = await res.arrayBuffer();
 	const p = new XRPackage(new Uint8Array(arrayBuffer));
-	pe.add(p);
+
+	await pe.add(p);
+
+	window.package = p
+
+	const frame = p.context.iframe.contentWindow
+
+	document.body
+		.addEventListener('keydown', (e) => frame.document.body.dispatchEvent(new KeyboardEvent('keydown', e)), true)
+	document.body
+		.addEventListener('keyup', (e) => frame.document.body.dispatchEvent(new KeyboardEvent('keyup', e)), true)
+	document.body
+		.addEventListener('keypress', (e) => frame.document.body.dispatchEvent(new KeyboardEvent('keypress', e)), true)
 }
